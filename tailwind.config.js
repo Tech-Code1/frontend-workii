657bad5af23736be7d49ca8479de4854136116d6
David Montoya <deividelpaisa@hotmail.com>
2022-10-25 21:20:24 -0500

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./projects/**/*.{html,ts}'],
+  important: true,
+  purge: {
+    content: ['./projects/**/*.{html,ts}'],
+  },
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily: {
-        title: ['Montserrat', 'sans-serif'],
-        body: ['Poppins', 'sans-serif']
-      },
+    fontFamily: {
+      title: ['Montserrat', 'sans-serif'],
+      body: ['Poppins', 'sans-serif']
+    },
 
+    extend: {
       colors: {
         primary: {
           blueNeutral: '#278CEA'
         },
+
         secondary: {
           lightBlue: '#E8F9FF'
         },
+
         accent: {
           yellow: '#FFD465'
         },
+
         gray: {
           lighter: '#F8F8F8',
           light: '#F1F1F1',
           neutral: '#D9D9D9'
         },
+
         white: {
           principal: '#FFFFFF',
           alternative: '#FDFDFD'
         },
+
         black: {
           principal: '#0E0D0D',
           alternative: '#333333'
@@ -36,5 +45,6 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ],
 }
