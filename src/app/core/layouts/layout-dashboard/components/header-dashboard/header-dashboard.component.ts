dc21ea3266346b5771b071c01f6090b0d0ae985d
David Montoya <deividelpaisa@hotmail.com>
2023-03-25 16:27:48 -0500

    user data async to info for dashboard added

--- src/app/core/layouts/layout-dashboard/components/header-dashboard/header-dashboard.component.ts
+++ src/app/core/layouts/layout-dashboard/components/header-dashboard/header-dashboard.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IAppState } from '../../../../state/app.state';
+import { selectOneUser } from '../../../../state/selectors/user.selectors';
+import { IUserDTO } from '../../../../models/user.interface';
 
 @Component({
   selector: 'header-dashboard',
@@ -9,11 +13,16 @@ import { Router } from '@angular/router';
 })
 export class HeaderDashboard implements OnInit {
 
+  private store = inject(Store<IAppState>)
+
+  user$!: Observable<IUserDTO | null>;
+
   constructor(
     private router:Router,
     ) { }
 
   ngOnInit(): void {
+    this.user$ = this.store.select(selectOneUser)
   }
 
 }
